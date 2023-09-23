c5d469a94dec1815a13a94a51f0a577b517f0f56
JorgeGonzales15 <u202118468@upc.edu.pe>
2023-09-23 16:41:33 -0500

    feat: added listbox and buttons to cost and profits section

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,40 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
+import ListBox from "@/Finance/components/listbox-content/ListBox.vue";
+export default {
+  name: "App",
+  components: {
+    ListBox,
+  },
+};
 
 </script>
 
 <template>
-  <header>
+  <div class="lista1 contenedor">
+    <list-box></list-box>
+  </div>
 
-
-    <div class="wrapper">
-
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
+
+.lista1{
+  background-color: #8f8d8d;
+  border-radius: 10px;
+  box-shadow: 0 4px 6px rgb(33, 33, 33);
+
+
+}
+.contenedor {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+  max-width: 50%;
+  margin: auto;
+
+
+}
 
 </style>
---
+++ src/Finance/components/listbox-content/ListBox.vue
@@ -0,0 +1,48 @@
+<template>
+
+  <div>
+    <pv-button label="Ver más costos" severity="success" />
+  </div>
+  <div class="card flex justify-content-center">
+    <h1></h1>
+    <pv-listbox v-model="selectedCity" :options="cities" filter optionLabel="name" class="w-full md:w-14rem" />
+
+  </div>
+  <img src="https://github.com/upc-pre-202302-GreatMinds-SW51/SW51-GreatMinds-OpenSource/assets/104078975/651be919-2573-45a3-b741-2d630f9debe6">
+
+  <div class="card flex justify-content-center">
+    <pv-listbox v-model="selectedCity" :options="cities" filter optionLabel="name" class="w-full md:w-14rem" />
+  </div>
+
+  <div>
+    <pv-button label="Ver más ganancias" severity="success" />
+  </div>
+
+
+</template>
+
+<script>
+export default  {
+  name: "list-box",
+  data() {
+    return {
+      selectedCity: null,
+      cities: [
+        { name: 'New York', code: 'NY' },
+        { name: 'Rome', code: 'RM' },
+        { name: 'London', code: 'LDN' },
+        { name: 'Istanbul', code: 'IST' },
+        { name: 'Paris', code: 'PRS' }
+      ]
+    };
+  }
+};
+
+</script>
+<style>
+.card {
+  padding: 1rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.1);
+}
+</style>
