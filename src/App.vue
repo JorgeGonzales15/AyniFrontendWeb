51ed7a1e417803940d5b3b48c8632137d4fe4cb3
JorgeGonzales15 <u202118468@upc.edu.pe>
2023-09-24 13:15:34 -0500

    feat: added buttons using routes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <script>
 import { RouterLink, RouterView } from 'vue-router'
 import ListBox from "@/Finance/components/listbox-content/ListBox.vue";
+import CardsCosts from "@/Finance/components/cost-main-content/Cards.vue";
+
 export default {
   name: "App",
   components: {
+    CardsCosts,
     ListBox,
+
+
   },
 };
 
 </script>
 
 <template>
-  <div class="lista1 contenedor">
-    <list-box></list-box>
+
+  <div class="background">
+    <div class="lista1 contenedor">
+      <list-box></list-box>
+    </div>
   </div>
+  <!-- <cards-costs></cards-costs>-->
+
 
 </template>
 
@@ -23,8 +33,6 @@ export default {
   background-color: #8f8d8d;
   border-radius: 10px;
   box-shadow: 0 4px 6px rgb(33, 33, 33);
-
-
 }
 .contenedor {
   display: flex;
@@ -33,8 +41,12 @@ export default {
   height: 80vh;
   max-width: 50%;
   margin: auto;
-
-
 }
+.background{
+  background: linear-gradient(to bottom, #2AAF87, #48D4AA, #ABEDD9, #D0F3CB, white);
+  background-size: cover;
+}
+
+
 
 </style>
