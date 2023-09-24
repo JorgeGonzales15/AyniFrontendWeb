46452435e4ee0246e916e24ca0aaa2b11c3cc0a4
Paolo Espejo <u20211b786@upc.edu.pe>
2023-09-24 13:47:17 -0500

    chore: Added crop routing and json.

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,75 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import stepperContent from "@/Management/crops/components/stepper-content.vue";
+import salesstepperContent from "@/Shopping/sales/components/stepper-content.vue";
+import salescardstep1Content from "@/Shopping/sales/components/card-step1-content.vue";
+import salescardstep2Content from "@/Shopping/sales/components/card-step2-content.vue";
+import salescardstep3Content from "@/Shopping/sales/components/card-step3-content.vue";
+import cropsstepperContent from "@/Management/crops/components/stepper-content.vue";
+import cropscardstep1Content from "@/Management/crops/components/card-step1-content.vue";
+import cropscardstep2Content from "@/Management/crops/components/card-step2-content.vue";
+import cropscardstep3Content from "@/Management/crops/components/card-step3-content.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-
     {
-      path: "/",
+      path: "/sales/",
       props: true,
-      component: stepperContent,
+      component: salesstepperContent,
       children: [
         {
-          path: "/",
+          path: "/sales/",
           name: "stepper-content",
           props: true,
-          component: () =>
-              import("@/Management/crops/components/stepper-content.vue")
+          component: salesstepperContent,
         },
         {
-          path: "/card-step1-content",
+          path: "/sales/step-1",
+          name:"sales-step1",
           props: true,
-          component: () =>
-                import("@/Management/crops/components/card-step1-content.vue")
+          component: salescardstep1Content,
         },
         {
-          path: "/card-step2-content",
+          path: "/sales/step-2",
+          name:"sales-step2",
           props: true,
-          component: () =>
-              import("@/Management/crops/components/card-step2-content.vue")
+          component: salescardstep2Content,
         },
         {
-          path: "/card-step3-content",
+          path: "/sales/step-3",
+          name:"sales-step3",
           props: true,
-          component: () =>
-              import("@/Management/crops/components/card-step3-content.vue")
+          component: salescardstep3Content,
+        },
+      ],
+    },
+    {
+      path: "/crops/",
+      props: true,
+      component: cropsstepperContent,
+      children: [
+        {
+          path: "/crops/",
+          name: "stepper-content",
+          props: true,
+          component: cropsstepperContent,
+        },
+        {
+          path: "/crops/step-1",
+          name:"crops-step1",
+          props: true,
+          component: cropscardstep1Content,
+        },
+        {
+          path: "/crops/step-2",
+          name:"crops-step2",
+          props: true,
+          component: cropscardstep2Content,
+        },
+        {
+          path: "/crops/step-3",
+          name:"crops-step3",
+          props: true,
+          component: cropscardstep3Content,
         },
       ],
     },
