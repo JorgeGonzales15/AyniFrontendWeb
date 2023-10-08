4c22ee9c7acf80e67b26c7b0a30d0e4770bcd0e9
TMedalith <u20211f955@upc.edu.pe>
2023-10-07 23:55:03 -0500

    style(product-card): Refine product card styling for better appearance

--- src/Shopping/products/components/product-card.component.vue
+++ src/Shopping/products/components/product-card.component.vue
@@ -16,3 +16,38 @@ export default {
   },
 };
 </script>
+<style scoped>
+.product-card {
+  background-color: rgba(35, 32, 32, 0.55);
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  padding: 20px;
+  overflow: hidden;
+  max-width: 250px;
+
+  @media (max-width: 767px) {
+    align-self: center;
+    max-width: 80%;
+  }
+}
+
+.product-card:hover {
+  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3);
+}
+
+.product-image {
+  width: 100%;
+  height: 110px;
+  object-fit: cover;
+  margin-bottom: 10px;
+}
+
+.product-name {
+  font-size: 16px;
+  margin: 0 auto;
+  padding: 5px 0;
+  width: 100%;
+  background-color: #3EAF2C;
+}
+</style>
