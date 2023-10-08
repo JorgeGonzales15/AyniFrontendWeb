569194548af0659e21667a5c5bb30f7377d42197
TMedalith <u20211f955@upc.edu.pe>
2023-10-07 23:54:01 -0500

    feat(product-card): Create product card component with basic structure

---
+++ src/Shopping/products/components/product-card.component.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="product-card">
+    <img class="product-image" :src="product.image_url" alt="product image_url"/>
+    <h2 class="product-name">{{ product.name }}</h2>
+  </div>
+</template>
