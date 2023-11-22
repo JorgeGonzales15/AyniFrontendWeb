5bcf68059cce02d3fe36b04ef9eb872cea923e98
Paolo Espejo <u20211b786@upc.edu.pe>
2023-11-22 17:37:48 -0500

    feat: Updated step 2
    Co-Authored-By: Huaman-Miguel <112042418+Huaman-Miguel@users.noreply.github.com>
    Co-Authored-By: Jorge Enrique Gonzales Carrión <104078975+JorgeGonzales15@users.noreply.github.com>
    Co-Authored-By: Tatiana Paucar <107322613+TMedalith@users.noreply.github.com>

--- src/Management/crops/components/card-step2-content.vue
+++ src/Management/crops/components/card-step2-content.vue
@@ -25,29 +25,35 @@
 </template>
 
 <script>
+// Import necessary validators and module for form validation
 import { required } from "@vuelidate/validators";
 import { useVuelidate } from "@vuelidate/core";
+
 export default {
   name: "card-step1-content",
+  // Setup function to initialize Vuelidate
   setup: () => ({ v$: useVuelidate() }),
   data() {
+    // Initialize component data
     return {
-      submitted: false,
-      watered: "",
-      pestcleaning: "",
+      submitted: false, // Flag to track whether the form has been submitted
+      watered: "", // Input field for watering information
+      pestcleaning: "", // Input field for pest cleaning information
     };
   },
   validations() {
+    // Vuelidate validations for each form field
     return {
       watered: {
-        required,
+        required, // Ensure that the watering field is not empty
       },
       pestcleaning: {
-        required,
+        required, // Ensure that the pest cleaning field is not empty
       },
     };
   },
   methods: {
+    // Method to emit an event to move to the next page
     nextPage() {
       this.$emit("next-page", {
         formData: {
@@ -57,12 +63,14 @@ export default {
         pageIndex: 1,
       });
     },
+    // Method to handle form submission
     handleSubmit(isFormValid) {
       this.submitted = true;
       if (isFormValid) {
-        this.nextPage();
+        this.nextPage(); // Move to the next page if the form is valid
       }
     },
+    // Method to emit an event to move to the previous page
     prevPage() {
       this.$emit("prev-page", { pageIndex: 1 });
     },
