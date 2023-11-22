11ef253256c8994e781943a86cadfa4c68d67034
Paolo Espejo <u20211b786@upc.edu.pe>
2023-11-22 17:30:37 -0500

    feat: Updated costs
    Co-Authored-By: Huaman-Miguel <112042418+Huaman-Miguel@users.noreply.github.com>
    Co-Authored-By: Jorge Enrique Gonzales Carrión <104078975+JorgeGonzales15@users.noreply.github.com>
    Co-Authored-By: Tatiana Paucar <107322613+TMedalith@users.noreply.github.com>

--- src/Finance/pages/costs.page.vue
+++ src/Finance/pages/costs.page.vue
@@ -32,41 +32,49 @@
 </template>
 
 <script>
-import {UserCostsApiService} from "@/Finance/services/userCosts-api.service";
+// Import necessary classes
+import { UserCostsApiService } from "@/Finance/services/userCosts-api.service";
 
 export default {
   name: "CostsPage",
   data() {
+    // Initialize component data
     return {
-      currentPage: 1,
-      pageSize: 6,
-      totalRecords: 0,
-      costs: [],
-      userCostsApi: new UserCostsApiService()
+      currentPage: 1, // Current page number for pagination
+      pageSize: 6, // Number of items to display per page
+      totalRecords: 0, // Total number of records (costs) available
+      costs: [], // Array to store the retrieved costs
+      userCostsApi: new UserCostsApiService() // Instance of the API service to manage user costs
     }
   },
   computed: {
+    // Computed property to get the subset of costs to be displayed on the current page
     displayedCosts() {
-      const start = (this.currentPage-1) * this.pageSize;
+      const start = (this.currentPage - 1) * this.pageSize;
       const end = this.pageSize + start;
       return this.costs.slice(start, end);
     },
+    // Computed property to get the current user from the state storage
     currentUser() {
       return this.$store.state.auth.user;
     }
   },
   methods: {
+    // Method triggered when the page changes in the pagination component
     onPageChange(event) {
       this.currentPage = event.page + 1;
     },
-    onReturn(){
+    // Navigate back to the select transaction page
+    onReturn() {
       this.$router.push("/select-transaction");
     },
-    onAddCost(){
+    // Navigate to the add cost page
+    onAddCost() {
       this.$router.push("/costs/add-cost");
     }
   },
   mounted() {
+    // Fetch all costs for the current user when the component is mounted
     this.userCostsApi.getAllCosts(this.currentUser.id).then((response) => {
       this.costs = response.data;
       this.totalRecords = this.costs.length;
